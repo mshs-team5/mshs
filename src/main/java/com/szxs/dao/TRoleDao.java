ac21b4b3d4c61385dd11114723def55ac37a12ed
Your Name <2036640596@qq.com>
2018-11-19 15:47:26 +0800

    删除

--- src/main/java/com/szxs/dao/TRoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.szxs.dao;
-
-import com.szxs.entity.TRoles;
-
-import java.util.List;
-
-public interface TRoleDao{
-    List<TRoles> queryAll();
-}
--- src/main/java/com/szxs/dao/TUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.szxs.dao;
-
-import com.szxs.entity.TUsers;
-import java.util.List;
-public interface TUserDao {
-      TUsers getUser(Integer id);
-      List<TUsers> queryAll();
-}
