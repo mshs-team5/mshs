9d98d0526b4e4699e395e1baedfe7bdec116ba26
2539902744 <2539902744@qq.com>
2018-11-20 09:19:01 +0800

    提交

---
+++ MshsProvider/src/main/java/com/szxs/dao/TActivityphotosDao.java
@@ -0,0 +1,11 @@
+package com.szxs.dao;
+
+import com.szxs.entity.TActivityphotos;
+
+import java.util.List;
+
+public interface TActivityphotosDao {
+    List<TActivityphotos> queryAll(TActivityphotos agent, Integer from , Integer end, Integer draw);
+
+
+}
