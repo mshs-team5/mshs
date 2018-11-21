ebc6b45ae47efa21e3a0e396ee615a319196c81c
Your Name <2036640596@qq.com>
2018-11-21 11:51:28 +0800

    新实体类

---
+++ Recoveryentity/src/main/java/com/szxs/entity/JqueryTable.java
@@ -0,0 +1,46 @@
+package com.szxs.entity;
+
+import java.util.List;
+
+/**
+ * ajax返回数据表
+ * @param <T>
+ */
+public class JqueryTable<T> {
+    private int draw;
+    private int recordsTotal;//总行数
+    private int recordsFiltered;//起始行数
+    private List<T> data;
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public int getRecordsTotal() {
+        return recordsTotal;
+    }
+
+    public void setRecordsTotal(int recordsTotal) {
+        this.recordsTotal = recordsTotal;
+    }
+
+    public int getRecordsFiltered() {
+        return recordsFiltered;
+    }
+
+    public void setRecordsFiltered(int recordsFiltered) {
+        this.recordsFiltered = recordsFiltered;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
--- Recoveryentity/src/main/java/com/szxs/service/AgentService.java
+++ Recoveryentity/src/main/java/com/szxs/service/AgentService.java
@@ -1,13 +1,15 @@
 package com.szxs.service;
 
 import com.szxs.entity.Agent;
+import com.szxs.entity.JqueryTable;
 
 import java.util.List;
 
 public interface AgentService{
-    List<Agent> queryAll(Agent agent,Integer from ,Integer end,Integer draw);
+    JqueryTable queryAll(Agent agent, Integer from , Integer end, Integer draw);
     Integer addAgent(Agent agent);
     Integer delAgent(Integer id);
     Integer updateAgent(Integer id);
     Agent queryByid(Integer id);
+    Integer queryAgentsRows(Agent agent);
 }
