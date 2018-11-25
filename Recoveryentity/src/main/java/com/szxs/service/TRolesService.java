8220e326e5f96f8702118fd6d31bd9bfa202c846
2539902744 <2539902744@qq.com>
2018-11-25 17:39:41 +0800

    111

--- Recoveryentity/src/main/java/com/szxs/service/TRolesService.java
+++ Recoveryentity/src/main/java/com/szxs/service/TRolesService.java
@@ -1,15 +1,15 @@
 package com.szxs.service;
 
-import com.szxs.entity.JqueryTable;
-import com.szxs.entity.TRoles;
-import com.szxs.entity.TUsers;
+import com.szxs.entity.*;
 
 import java.util.List;
 
 public interface TRolesService {
 
     JqueryTable<TRoles> queryAll(TRoles tRoles, Integer from , Integer end, Integer draw);
-    Integer addTRoles(TRoles tRoles);
-    Integer delTRoles(String uuid);
-    Integer updateTRoles(TRoles tRoles);
+    Integer addtttRoles(TRoles tRoles);
+    Integer deltttRoles(String  uuid);
+    Integer updatetttRoles(TRoles tRoles);
+    TDevuser queryByid(String id);
+    Integer querytttRolesRows(TRoles tRoles);
 }
