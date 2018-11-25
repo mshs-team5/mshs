eef2169362a3bfec24f0de16de59ad5b26f9bc8f
Your Name <2036640596@qq.com>
2018-11-25 17:50:04 +0800

    修改生产者

--- MshsProvider/src/main/java/com/szxs/service/impl/AgentServiceImpl.java
+++ MshsProvider/src/main/java/com/szxs/service/impl/AgentServiceImpl.java
@@ -5,6 +5,7 @@ import com.szxs.entity.Agent;
 import com.szxs.entity.JqueryTable;
 import com.szxs.service.AgentService;
 import org.springframework.stereotype.Service;
+import sun.management.resources.agent;
 
 import javax.annotation.Resource;
 @Service
@@ -12,11 +13,7 @@ public class AgentServiceImpl implements AgentService {
     @Resource
     private AgentDao agentDao;
     public JqueryTable queryAll(Agent agent, Integer from, Integer end, Integer draw) {
-<<<<<<< Updated upstream
         JqueryTable<Agent> table = new JqueryTable();
-=======
-        JqueryTable<Agent> table = new JqueryTable<Agent> ();
->>>>>>> Stashed changes
         table.setDraw(draw);
         table.setRecordsTotal(agentDao.queryAgentsRows(agent));
         table.setRecordsFiltered(table.getRecordsTotal());
@@ -26,26 +23,23 @@ public class AgentServiceImpl implements AgentService {
     public Integer addAgent(Agent agent) {
         return null;
     }
-<<<<<<< Updated upstream
 
-    public Integer delAgent(String s) {
-        return null;
-    }
 
-=======
+
+
+
     public Integer delAgent(String uuid) {
         return null;
     }
->>>>>>> Stashed changes
+
     public Integer updateAgent(Agent agent) {
         return null;
     }
 
-<<<<<<< Updated upstream
-    public Agent queryByid(String s) {
-=======
+
+
     public Agent queryByid(String uuid) {
->>>>>>> Stashed changes
+
         return null;
     }
 
