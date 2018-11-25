20c79d98360bbb89f0f0a9759ee7aca5f0f51d5a
2539902744 <2539902744@qq.com>
2018-11-25 14:29:01 +0800

    测试用户

--- Recoveryentity/src/main/java/com/szxs/service/TdevuserService.java
+++ Recoveryentity/src/main/java/com/szxs/service/TdevuserService.java
@@ -1,5 +1,6 @@
 package com.szxs.service;
 
+import com.szxs.entity.Agent;
 import com.szxs.entity.JqueryTable;
 import com.szxs.entity.TDevuser;
 
@@ -7,4 +8,10 @@ import java.util.List;
 
 public interface TdevuserService{
     JqueryTable<TDevuser> queryAll(TDevuser tDevuser, Integer from , Integer end, Integer draw);
+
+    Integer addtDevuser(TDevuser tDevuser);
+    Integer deltDevuser(Integer id);
+    Integer updatetDevuser(Integer id);
+    Agent queryByid(Integer id);
+    Integer querytDevuserRows(TDevuser tDevuser);
 }
