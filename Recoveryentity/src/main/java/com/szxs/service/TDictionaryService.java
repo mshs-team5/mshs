5001bc834615925d41e28ea03c9dc16b8dc5a344
Your Name <2036640596@qq.com>
2018-11-26 14:34:02 +0800

    没改啥

--- Recoveryentity/src/main/java/com/szxs/service/TDictionaryService.java
+++ Recoveryentity/src/main/java/com/szxs/service/TDictionaryService.java
@@ -1,16 +1,12 @@
 package com.szxs.service;
 
 import com.szxs.entity.JqueryTable;
-import com.szxs.entity.TDevuser;
 import com.szxs.entity.TDictionary;
-
-import java.util.List;
-
 public interface TDictionaryService {
     JqueryTable<TDictionary> queryAll(TDictionary tDictionary, Integer from , Integer end, Integer draw);
     Integer addttDictionary(TDictionary tDictionary);
     Integer delttDictionary(String  uuid);
     Integer updatettDictionary(TDictionary tDictionary);
-    TDevuser queryByid(String id);
+    TDictionary queryByid(String id);
     Integer queryttDictionaryRows(TDictionary tDictionary);
 }
