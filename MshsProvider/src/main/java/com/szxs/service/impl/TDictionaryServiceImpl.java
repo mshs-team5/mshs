bc06acc0e784deaf3dd633f5bddf65825fb33b62
2539902744 <2539902744@qq.com>
2018-11-26 14:54:13 +0800

    666666666666

--- MshsProvider/src/main/java/com/szxs/service/impl/TDictionaryServiceImpl.java
+++ MshsProvider/src/main/java/com/szxs/service/impl/TDictionaryServiceImpl.java
@@ -3,6 +3,7 @@ package com.szxs.service.impl;
 import com.szxs.dao.TDictionaryDao;
 import com.szxs.entity.JqueryTable;
 import com.szxs.entity.TDictionary;
+import com.szxs.entity.TRoles;
 import com.szxs.service.TDictionaryService;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
@@ -12,9 +13,33 @@ import java.util.List;
 public class TDictionaryServiceImpl implements TDictionaryService {
     @Resource
     public TDictionaryDao tDictionaryDao;
-    public JqueryTable<TDictionary> queryAll(TDictionary tDictionary, Integer integer, Integer integer1, Integer integer2) {
 
-        JqueryTable<TDictionary> table=new JqueryTable<TDictionary>();
+    public JqueryTable<TDictionary> queryAll(TDictionary tDictionary,  Integer from, Integer end, Integer draw) {
+        JqueryTable<TDictionary> table = new JqueryTable();
+        table.setDraw(draw);
+        table.setRecordsTotal(tDictionaryDao.queryttDictionaryRows(tDictionary));
+        table.setRecordsFiltered(table.getRecordsTotal());
+        table.setData(tDictionaryDao.queryAll(tDictionary,from,end));
         return table;
+    }
+
+    public Integer addttDictionary(TDictionary tDictionary) {
+        return tDictionaryDao.addttDictionary(tDictionary);
+    }
+
+    public Integer delttDictionary(String s) {
+        return tDictionaryDao.delttDictionary(s);
+    }
+
+    public Integer updatettDictionary(TDictionary tDictionary) {
+        return tDictionaryDao.updatettDictionary(tDictionary);
+    }
+
+    public TDictionary queryByid(String s) {
+        return null;
+    }
+
+    public Integer queryttDictionaryRows(TDictionary tDictionary) {
+        return tDictionaryDao.queryttDictionaryRows(tDictionary);
     }
 }
---
+++ MshsProvider/src/main/java/com/szxs/service/impl/TRolesServiceImpl.java
@@ -0,0 +1,44 @@
+package com.szxs.service.impl;
+
+import com.szxs.dao.TRoleDao;
+import com.szxs.entity.Agent;
+import com.szxs.entity.JqueryTable;
+import com.szxs.entity.TRoles;
+import com.szxs.service.TRolesService;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.List;
+@Service
+public class TRolesServiceImpl implements TRolesService {
+    @Resource
+    private TRoleDao tRoleDao;
+
+    public JqueryTable<TRoles> queryAll(TRoles tRoles, Integer from, Integer end, Integer draw) {
+        JqueryTable<TRoles> table = new JqueryTable();
+        table.setDraw(draw);
+        table.setRecordsTotal(tRoleDao.querytttRolesRows(tRoles));
+        table.setRecordsFiltered(table.getRecordsTotal());
+        table.setData(tRoleDao.queryAll(tRoles,from,end));
+        return table;
+    }
+
+    public Integer addtttRoles(TRoles tRoles) {
+        return tRoleDao.addtttRoles(tRoles);
+    }
+
+    public Integer deltttRoles(String s) {
+        return tRoleDao.deltttRoles(s);
+    }
+
+    public Integer updatetttRoles(TRoles tRoles) {
+        return tRoleDao.updatetttRoles(tRoles);
+    }
+
+    public TRoles queryByid(String s) {
+        return null;
+    }
+
+    public Integer querytttRolesRows(TRoles tRoles) {
+        return tRoleDao.querytttRolesRows(tRoles);
+    }
+}
